bbcee51a99f55bb87b893d4fe90303e25376f324
Rehan Faisal <124900846+rehan-faisal-cys@users.noreply.github.com>
2024-11-12 07:05:10 +0500

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -10,9 +10,9 @@ cred_file = './files/industrial-glow-430302-b9-2614cd5ccb2b.json'
 
 files = [url_file , cred_file , './files/notifications_flag.json' ,'./files/sheet_id.json']
 if check_all_files(files):
-    email_sender = 'viralvideoapp7@gmail.com'
-    email_password = 'ouctkcvrucyhruxr'
-    email_receiver = ['viralvideoapp7@gmail.com','ajmuro19@gmail.com','captainalders1@gmail.com','Jonmuro10x@icloud.com']
+    email_sender = ''
+    email_password = ''
+    email_receiver = ['test1@gmail.com','test2@gmail.com','test3@gmail.com','test4@icloud.com']
     schedule.every().day.at("00:01").do(lambda: create_sheet(cred_file))
     schedule.every(5).minutes.do(lambda: main(url_file,email_sender, email_password, email_receiver,cred_file))
     while True:
